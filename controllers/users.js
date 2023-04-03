7b1203cbb1c123f7e4d7d58f8d128e7772392dd6
momot <motraore611@yahoo.com>
2023-04-03 13:30:37 -0400

    return to relational model

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const User = require('../models/users.js')
+const Post = require('../models/posts.js')
 
 router.post('/', (req, res) => {
     User.create(req.body).then((createdUser) => {
